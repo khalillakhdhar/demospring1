6cd7da3cb0963c70956cfda9c090739b914affec
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-10-23 14:37:14 +0100

    update

--- src/main/java/com/example/demo/controller/DemoControllers.java
+++ src/main/java/com/example/demo/controller/DemoControllers.java
@@ -1,5 +1,20 @@
 package com.example.demo.controller;
 
-public class DemoControllers {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class DemoControllers {
+	@GetMapping("/api")
+	public String getFoos(@RequestParam String id) {
+	return "ID: " + id;
+	}
+	@PostMapping("/api")
+	public String addFoo(@RequestParam(name = "id") String fooId,
+	@RequestParam String name) {
+	return "ID: " + fooId + " Name: " + name;
+	}
 }
